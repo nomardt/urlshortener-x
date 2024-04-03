2055352e0b40d8ffb12bae6be2941a51948ccd5e
nomardt <nomardt@aol.com>
2024-04-03 21:38:38 +0300

    Switched to Chi server framework

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,74 +2,74 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/nomardt/urlshortener-x/internal/idgenerator"
 )
 
-type uriStorage map[string]string
+// Declaring the map to store shortened URLs in
+type UriStorage map[string]string
 
-var storage uriStorage
+var storage UriStorage
 
-func main() {
-	if err := run(); err != nil {
-		panic(err)
+func newUriHandler(storage UriStorage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Get the body value
+		body, err := io.ReadAll(r.Body)
+		if err != nil || string(body) == "" {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+			return
+		}
+
+		// Check if the body value is an actual URL
+		u, err := url.ParseRequestURI(string(body))
+		hostname := u.Host
+		if err != nil || (u.Scheme != "http" && u.Scheme != "https") || !strings.Contains(hostname, ".") || string(hostname[0]) == "." || string(hostname[len(hostname)-1]) == "." {
+			http.Error(w, "Please enter a valid URL", http.StatusBadRequest)
+			return
+		}
+
+		// Add the received URL to storage
+		randomID := idgenerator.GenerateRandomID(8)
+		storage[randomID] = u.String()
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte("http://localhost:8080/" + randomID))
 	}
 }
 
-func run() error {
-	storage = make(uriStorage)
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", mainPageHandler(storage))
-
-	return http.ListenAndServe(`:8080`, mux)
-}
-
-func mainPageHandler(storage uriStorage) http.HandlerFunc {
+func getUriHandler(storage UriStorage, unitTestID ...string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		id := r.URL.Path[1:]
+		id := chi.URLParam(r, "id")
 
-		if r.Method == http.MethodPost && id == "" {
-			// Check the Content-Type value
-			contentType := r.Header.Get("Content-Type")
-			if !strings.HasPrefix(contentType, "text/plain") {
-				http.Error(w, "Invalid Content-Type! Please use text/plain", http.StatusBadRequest)
-				return
-			}
+		// This is necessary because chi.URLParam doesn't parse IDs from unit tests for some reason
+		if len(unitTestID) > 0 {
+			id = unitTestID[0]
+		}
 
-			// Get the body value
-			body, err := io.ReadAll(r.Body)
-			if err != nil {
-				http.Error(w, "Bad Request", http.StatusBadRequest)
-				return
-			}
-
-			// Check if the body value is an actual URL
-			u, err := url.ParseRequestURI(string(body))
-			hostname := u.Host
-			if err != nil || (u.Scheme != "http" && u.Scheme != "https") || !strings.Contains(hostname, ".") || string(hostname[0]) == "." || string(hostname[len(hostname)-1]) == "." {
-				http.Error(w, "Please enter a valid URL", http.StatusBadRequest)
-				return
-			}
-
-			// Add the received URL to storage
-			randomID := idgenerator.GenerateRandomID(8)
-			storage[randomID] = u.String()
-
-			w.WriteHeader(http.StatusCreated)
-			w.Write([]byte("http://localhost:8080/" + randomID))
-		} else if r.Method == http.MethodGet {
-			if url, exists := storage[id]; exists {
-				w.Header().Set("Location", url)
-				w.Header().Set("Content-Type", "application/json")
-				w.WriteHeader(http.StatusTemporaryRedirect)
-			} else {
-				http.Error(w, "URL not found on the server!", http.StatusBadRequest)
-			}
+		if url, exists := storage[id]; exists {
+			w.Header().Set("Location", url)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusTemporaryRedirect)
 		} else {
-			http.Error(w, "Bad Request", http.StatusBadRequest)
+			http.Error(w, "URL not found on the server!", http.StatusBadRequest)
 		}
 	}
+}
+
+func main() {
+	storage = make(UriStorage)
+	r := chi.NewRouter()
+
+	r.Use(middleware.AllowContentType("text/plain"))
+
+	r.Post("/", newUriHandler(storage))
+	r.Get("/{id}", getUriHandler(storage))
+
+	log.Fatal(http.ListenAndServe(`:8080`, r))
 }
