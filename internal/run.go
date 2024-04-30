d5fa36f69c980d1d0068ca18c5518f480ffa12a0
nomardt <nomardt@aol.com>
2024-04-30 23:21:29 +0300

    update: added permanent URL storage on disk

--- internal/run.go
+++ internal/run.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -24,6 +25,13 @@ func Run(config conf.Configuration) error {
 	router.Use(middleware.Compress(3))
 
 	urlsRepo := urlsInfra.NewInMemoryRepo()
+	if err := urlsRepo.LoadStoredURLs(config); err != nil {
+		logger.Log.Info("Couldn't recover any previously shortened URLs!", zap.String("error", err.Error()))
+
+		if _, err := os.Create(config.StorageFile); err != nil {
+			logger.Log.Info("No file will be created to store shortened URLs")
+		}
+	}
 	urls.Setup(router, urlsRepo, config)
 
 	logger.Log.Info("The server has started", zap.String("address", config.ListenAddress))
